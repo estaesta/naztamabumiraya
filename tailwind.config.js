5dd25c5165015ea4d4ad2191caf67286155ab205
estaesta <ahmadforesta@gmail.com>
2023-06-11 08:52:59 +0700

    feat: portofolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    // require('taos/plugin')
+  ],
   important: true,
+  // safelist: [
+  // '!duration-[0ms]',
+  // '!delay-[0ms]',
+  // 'html.js :where([class*="taos:"]:not(.taos-init))'
+  // ],
+  // content: {
+  //   transform: (content) => content.replace(/taos:/g, ''),
+  // },
 }
 
